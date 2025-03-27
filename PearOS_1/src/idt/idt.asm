8c57843027ab99d424f322fe76c06b29589d49d4
Bishwash Karki <bishwash.karki12@gmail.com>
2025-03-27 16:06:57 -0500

    Fixed enable interrupts location to avoid kernel panic incase interrupt is raised before initilazing IDT

--- PearOS_1/src/idt/idt.asm
+++ PearOS_1/src/idt/idt.asm
@@ -9,6 +9,17 @@ extern no_interrupt_handler
 
 global idt_load
 
+global enable_interrupts
+global disable_interrupts
+
+enable_interrupts:
+    sti
+    ret
+
+disable_interrupts:
+    cli
+    ret
+
 idt_load:
     push ebp
     mov ebp, esp

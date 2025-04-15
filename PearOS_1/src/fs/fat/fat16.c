ab4a22ed86bbc7d0b4948b3761a35e933754dcc7
Bishwash Karki <bishwash.karki12@gmail.com>
2025-04-15 16:25:48 -0500

    Continuing work on FAT resolve function

--- PearOS_1/src/fs/fat/fat16.c
+++ PearOS_1/src/fs/fat/fat16.c
@@ -2,6 +2,7 @@
 #include "string/string.h"
 #include "status.h"
 #include <stdint.h>
+#include <math.h>
 #include "disk/disk.h"
 #include "disk/streamer.h"
 #include "memory/memory.h"
@@ -148,6 +149,104 @@ static void fat16_init_private(struct disk* disk, struct fat_private* private)
     private->directory_stream = diskstreamer_new(disk->id);
 }
 
+int fat16_sector_to_absolute(struct disk* disk, int sector)
+{
+    return sector * disk->sector_size;
+}
+
+int fat16_get_total_items_for_directory(struct disk* disk, uint32_t directory_start_sector)
+{
+    struct fat_directory_item item;
+    struct fat_directory_item empty_item;
+    memset(&empty_item, 0, sizeof(empty_item));
+
+    struct fat_private* fat_private = disk->fs_private;
+
+    int res = 0;
+    int i = 0;
+
+    int directory_start_pos = directory_start_sector * disk->sector_size;
+    struct disk_stream* stream = fat_private->directory_stream;
+    if (diskstreamer_seek(stream, directory_start_pos) != PEAROS_ALL_OK)
+    {
+        res = -EIO;
+        goto out;
+    }
+
+    while (1)
+    {
+        if (diskstreamer_read(stream, &item, sizeof(item)) != PEAROS_ALL_OK)
+        {
+            res = -EIO;
+            goto out;
+        }
+
+        if (item.filename[0] == 0x00)
+        {
+            // We are done
+            break;
+        }
+
+        // Is this item unused
+        if (item.filename[0] == 0xE5)
+        {
+            continue;
+        }
+
+        i++;
+    }
+
+    res = i;
+
+out:
+    return res;
+}
+
+int fat16_get_root_directory(struct disk* disk, struct fat_private* fat_private, struct fat_directory* directory)
+{
+    int res = 0;
+    struct fat_header* primary_header = &fat_private->header.primary_header;
+    int root_dir_sector_pos = (primary_header->fat_copies * primary_header->sectors_per_fat) + primary_header->reserved_sectors;
+    int root_dir_entries = fat_private->header.primary_header.root_dir_entries;
+    int root_dir_size = (root_dir_entries * sizeof(struct fat_directory_item));
+    int total_sectors = (root_dir_size / disk->sector_size);
+    if ((root_dir_size) % (disk->sector_size))
+    {
+        total_sectors += 1;
+    }
+
+    int total_items = fat16_get_total_items_for_directory(fat_private, root_dir_sector_pos);
+
+    struct fat_directory_item* dir = kzalloc(root_dir_size);
+    if (!dir)
+    {
+        res = -ENOMEM;
+        goto out;
+    }
+
+    struct disk_stream* stream = fat_private->directory_stream;
+    if (diskstreamer_seek(stream, fat16_sector_to_absolute(disk, root_dir_sector_pos)) != PEAROS_ALL_OK)
+    { 
+        res = -EIO;
+        goto out;
+    }
+
+    if (diskstreamer_read(stream, dir, root_dir_size) != PEAROS_ALL_OK)
+    {
+        res = -EIO;
+        goto out;
+    }
+
+    directory->item = dir;
+    directory->total = total_items;
+    directory->sector_pos = root_dir_sector_pos;
+    directory->ending_sector_pos = root_dir_sector_pos + (root_dir_size / disk->sector_size);
+
+
+out:
+    return res;    
+}
+
 int fat16_resolve(struct disk* disk)
 {
     int res = 0;
@@ -178,6 +277,8 @@ int fat16_resolve(struct disk* disk)
         goto out;
     }
 
+    disk->fs_private = fat_private;
+    disk->filesystem = &fat16_fs;
 
 out:
     return res;

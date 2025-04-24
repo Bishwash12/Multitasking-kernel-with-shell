6540b93fab268716d72b346aa4c634db2a4a1669
Bishwash Karki <bishwash.karki12@gmail.com>
2025-04-24 13:54:40 -0500

    Implemented task foundations

--- PearOS_1/src/memory/paging/paging.h
+++ PearOS_1/src/memory/paging/paging.h
@@ -21,6 +21,8 @@ struct paging_4gb_chunk
 
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk);
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags);
+void paging_free_4gb(struct paging_4gb_chunk* chunk);
+
 void paging_switch(uint32_t* directory);
 void enable_paging();
 bool paging_is_aligned(void* addr);

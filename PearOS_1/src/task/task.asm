c0b24bb921b6234be08e901e126d0b92c842fc0d
Bishwash Karki <bishwash.karki12@gmail.com>
2025-04-30 10:31:16 -0500

    Completed User land functionality

--- PearOS_1/src/task/task.asm
+++ PearOS_1/src/task/task.asm
@@ -3,6 +3,8 @@
 section .asm
 
 global restore_general_purpose_registers
+global task_return
+global user_registers
 
 task_return:
     mov ebp, esp
@@ -14,7 +16,36 @@ task_return:
 
     ; Let's access the structure passed to us
     mov ebx, [ebp+4]
-    push dword
+    ; Push the data/stack selector
+    push dword [ebx+44]
+    ; Push the stack pointer
+    push dword [ebx+40]
+
+    ; Push the flags
+    pushf
+    pop eax
+    or eax, 0x200
+    push eax
+
+    ; Push the code segment
+    push dword [ebx+32]
+
+    ; Push the IP to execute
+    push dword [ebx+28]
+
+    ; Setup some segment registers
+    mov ax, [ebx+44]
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    push dword [ebx+4]
+    call restore_general_purpose_registers
+    add esp, 4
+
+    ; Let's leave kernel land and execute in user land
+    iretd
 
 restore_general_purpose_registers:
     push ebp
@@ -28,4 +59,13 @@ restore_general_purpose_registers:
     mov eax, [ebx+24]
     mov ebx, [ebx+12]
     pop ebp
+    ret
+
+; void user_registers()
+user_registers:
+    mov ax, 0x23
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
     ret

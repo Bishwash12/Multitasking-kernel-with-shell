6e385dca81bc08fc1fd44b99b9fee723da6df1ca
Bishwash Karki <bishwash.karki12@gmail.com>
2025-05-05 09:43:36 -0500

    Created ability to create and execute kernel commands

--- PearOS_1/src/task/task.c
+++ PearOS_1/src/task/task.c
@@ -112,7 +112,18 @@ int task_switch(struct task* task)
 
 int task_save_state(struct task* task, struct interrupt_frame* frame)
 {
-
+    task->registers.ip = frame->ip;
+    task->registers.cs = frame->cs;
+    task->registers.flags = frame->flags;
+    task->registers.esp = frame->esp;
+    task->registers.ss = frame->ss;
+    task->registers.eax = frame->eax;
+    task->registers.ebp = frame->ebp;
+    task->registers.ebx = frame->ebx;
+    task->registers.ecx = frame->ecx;
+    task->registers.edi = frame->edi;
+    task->registers.edx = frame->edx;
+    task->registers.esi = frame->esi;
 }
 
 void task_current_save_state(struct interrupt_frame* frame)

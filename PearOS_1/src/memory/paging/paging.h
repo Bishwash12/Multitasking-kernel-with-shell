5be365b7e94be3e87a1cfad6236ff00000dd080e
Bishwash Karki <bishwash.karki12@gmail.com>
2025-05-06 11:41:00 -0500

    Created function to copy string from task

--- PearOS_1/src/memory/paging/paging.h
+++ PearOS_1/src/memory/paging/paging.h
@@ -33,6 +33,7 @@ int paging_set(uint32_t* directory, void* virt, uint32_t val);
 int paging_map_to(struct paging_4gb_chunk *directory, void *virt, void *phys, void *phys_end, int flags);
 int paging_map_range(struct paging_4gb_chunk* directory, void* virt, void* phys, int count, int flags);
 int paging_map(struct paging_4gb_chunk* directory, void* virt, void* phys, int flags);
+uint32_t paging_get(uint32_t* directory, void* virt);
 
 
 #endif

1178c6cea84a5ef217c670200f47bbb67ebe9a8e
Bishwash Karki <bishwash.karki12@gmail.com>
2025-05-27 12:57:59 -0500

    Started implementing ELF Loader

---
+++ PearOS_1/src/loader/formats/elf.h
@@ -0,0 +1,50 @@
+#ifndef ELF_H
+#define ELF_H
+
+#define PF_X 0x01
+#define PF_W 0x02
+#define PF_R 0x04
+
+#define PT_NULL 0
+#define PT_LOAD 1
+#define PT_DYNAMIC 2
+#define PT_INTERP 3
+#define PT_NOTE 4
+#define PT_SHLIB 5
+#define PT_PHDR 6
+
+#define SHT_NULL 0
+#define SHT_PROGBITS 1
+#define SHT_SYMTAB 2
+#define SHT_STRTAB 3
+#define SHT_RELA 4
+#define SHT_HASH 5
+#define SHT_DYNAMIC 6
+#define SHT_NOTE 7
+#define SHT_NOBITS 8
+#define SHT_REL 9
+#define SHT_SHLIB 10
+#define SHT_DYNSYM 11
+#define SHT_LOPROC 12
+#define SHT_HIPROC 13
+#define SHT_LOUSER 14
+#define SHT_HIUSER 15
+
+#define ET_NONE 0
+#define ET_REL 1
+#define ET_EXEC 2
+#define ET_DYN 3
+#define ET_CORE 4
+
+#define EI_NIDENT 16
+#define EI_CLASS 4
+#define EI_DATA 5
+
+#define ELFCLASSNONE 0
+#define ELFCLASS32 1
+#define ELFCLASS64 2
+
+#define ELFDATANONE 0
+#define ELFDATA2LSB 1
+#define ELF
+#endif

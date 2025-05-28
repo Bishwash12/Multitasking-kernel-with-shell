80922e39bf62d09631496cb0c9a808c8a470de1b
Bishwash Karki <bishwash.karki12@gmail.com>
2025-05-28 09:48:14 -0500

    Complete elf loader part 1 where we defined structures and headers for elf loader

--- PearOS_1/src/loader/formats/elf.h
+++ PearOS_1/src/loader/formats/elf.h
@@ -1,6 +1,9 @@
 #ifndef ELF_H
 #define ELF_H
 
+#include <stdint.h>
+#include <stddef.h>
+
 #define PF_X 0x01
 #define PF_W 0x02
 #define PF_R 0x04
@@ -46,5 +49,79 @@
 
 #define ELFDATANONE 0
 #define ELFDATA2LSB 1
-#define ELF
+#define ELFDATA2MSB 2
+
+#define SHN_UNDEF 0
+
+typedef uint16_t elf32_half;
+typedef uint32_t elf32_word;
+typedef int32_t elf32_sword;
+typedef uint32_t elf32_addr;
+typedef int32_t elf32_off;
+
+struct elf32_phdr
+{
+    elf32_word p_type;
+    elf32_off p_offset;
+    elf32_addr p_vaddr;
+    elf32_addr p_paddr;
+    elf32_word p_filesz;
+    elf32_addr p_memsz;
+    elf32_addr p_flags;
+    elf32_addr p_align;
+} __attribute__((packed));
+
+struct elf32_shdr
+{
+    elf32_word sh_name;
+    elf32_word sh_type;
+    elf32_word sh_flags;
+    elf32_addr sh_addr;
+    elf32_off sh_offset;
+    elf32_word sh_size;
+    elf32_word sh_link;
+    elf32_word sh_info;
+    elf32_word sh_addralign;
+    elf32_word sh_entsize;
+} __attribute__((packed));
+
+struct elf_header
+{
+    unsigned char e_ident[EI_NIDENT];
+    elf32_half e_type;
+    elf32_half e_machine;
+    elf32_word e_version;
+    elf32_addr e_entry;
+    elf32_off e_phoff;
+    elf32_off e_shoff;
+    elf32_word e_flags;
+    elf32_half e_ehsize;
+    elf32_half e_phentsize;
+    elf32_half e_phnum;
+    elf32_half e_shentsize;
+    elf32_half e_shnum;
+    elf32_half e_shstrndx;
+} __attribute__((packed));
+
+struct elf32_dyn
+{
+    elf32_sword d_tag;
+    union
+    {
+        elf32_word d_val;
+        elf32_addr d_ptr;
+    } d_un;
+} __attribute__((packed));
+
+struct elf32_sym
+{
+    elf32_word st_name;
+    elf32_addr st_value;
+    elf32_word st_size;
+    unsigned char st_info;
+    unsigned char st_other;
+    elf32_half st_shndx;
+} __attribute__((packed));
+
+
 #endif

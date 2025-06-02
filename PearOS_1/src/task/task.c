367ea2ceb64aafb2822ed832e414161fd2f38621
Bishwash Karki <bishwash.karki12@gmail.com>
2025-06-02 12:24:43 -0500

    Completed elf loader and started working on writing user programs in C

--- PearOS_1/src/task/task.c
+++ PearOS_1/src/task/task.c
@@ -7,6 +7,7 @@
 #include "idt/idt.h"
 #include "memory/paging/paging.h"
 #include "string/string.h"
+#include "loader/formats/elfloader.h"
 
 // The current task that is running
 struct task* current_task = 0;
@@ -213,6 +214,11 @@ int task_init(struct task* task, struct process* process)
     }
 
     task->registers.ip = PEAROS_PROGRAM_VIRTUAL_ADDRESS;
+
+    if (process->filetype == PROCESS_FILE_TYPE_ELF)
+    {
+        task->registers.ip = elf_header(process->elf_file)->e_entry;
+    }
     task->registers.ss = USER_DATA_SEGMENT;
     task->registers.cs = USER_CODE_SEGMENT;
     task->registers.esp = PEAROS_PROGRAM_VIRTUAL_STACK_ADDRESS_START;

d99a13ef5dec65988952c468670dd44b80e6d8f3
Bishwash Karki <bishwash.karki12@gmail.com>
2025-06-11 09:20:30 -0500

    Parsing arguments part 1, splitting the arguments

--- PearOS_1/programs/stdlib/src/pearos.h
+++ PearOS_1/programs/stdlib/src/pearos.h
@@ -3,6 +3,14 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+struct command_argument
+{
+    char argument[512];
+    struct command_argument* next;
+};
+
+struct command_argument* pearos_parse_command(const char* command, int max);
+
 void print(const char* message);
 int pearos_getkey();
 void* pearos_malloc(size_t size);
